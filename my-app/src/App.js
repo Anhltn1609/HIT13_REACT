79533f7b841064209568e88003f26c5590d4af04
Anhltn1609 <Anhsltn1234r@gmail.com>
2023-04-09 23:06:34 +0700

    btvnb5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,10 @@
 // import logo from './logo.svg';
 import "./App.css";
-// import YoutubeItem from "./component/YoutubeItem/YoutubeItem";
-// import {YoutubeData} from './component/YoutubeItem/Data';
-// import Toggle from "./component/YoutubeItem/state/Toggle";
-import Tictoctoe from "./component/tictoctoe/Tictoctoe";
+import Photos from "./component/photo";
 function App() {
-  // console.log(YoutubeData)
-  // console.log("hehe")
   return (
     <div>
-      <Tictoctoe></Tictoctoe>
-      {/* <div>hehehe</div> */}
+    <Photos></Photos>
     </div>
 
   );
